7fb43ab5e2c6867f905c78d226922ecffb6a1df7
murilojcavalcanti <josemurilo7@gmail.com>
2024-05-13 20:30:07 -0300

    adicionando o uso de docker

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -5,23 +5,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao
 {
     //Essa classe serve para criarmos apenas uma instancia do context, assim não iremos criar diversas conexões com o banco de dados.
-    public class ContextoFixture
+    public class ContextoFixture //: IAsyncLifetime<- Interface usada para conseguir usar o docker nos tests
     {
-        public JornadaMilhasContext Context { get; }
+        public JornadaMilhasContext Context;
+        private readonly MsSqlContainer msSqlContainer = new MsSqlBuilder().WithImage("mcr.microsoft.com/mssql/server").Build(); 
 
         public ContextoFixture()
         {
+            
             var options = new DbContextOptionsBuilder<JornadaMilhasContext>
             ().UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;" +
             "Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;" +
             "Application Intent=ReadWrite;Multi Subnet Failover=False").Options;
-            
             Context = new JornadaMilhasContext(options);
+            
 
         }
+        /*
+         * usando o docker
+        public async Task InitializeAsync()
+        {
+            await msSqlContainer.StartAsync();
+            var options = new DbContextOptionsBuilder<JornadaMilhasContext>
+           ().UseSqlServer(msSqlContainer.GetConnectionString()).Options;
+
+            
+            Context = new JornadaMilhasContext(options);
+            Context.Database.Migrate();
+            
+        }
+
+        public async Task DisposeAsync()
+        {
+            await msSqlContainer.StopAsync();
+        }
+        */
     }
 }
